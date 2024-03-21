fa251f85cdd16d1a8052475850041f6524862d42
dariamm03 <164412814+dariamm03@users.noreply.github.com>
2024-03-22 02:06:18 +0500

    Add files via upload

---
+++ site/app/include/footer.php
@@ -0,0 +1,28 @@
+<!-- Footer -->
+<div class="footer container-fluid">
+    <div class="footer-content container">
+        <div class="row">
+            <div class="footer-section about">
+                <h3 class="logo-text">Подготовка к экзамену по морскому английскому</h3>
+                <div class="socials">
+                    <p>
+                        КОНТАКТЫ
+                    </p>
+                    <a href="#"><i class="fas fa-phone"></i> &nbsp; +7(342)279-91-78</a>
+                    <p> <br/>
+                        ул. Газеты Звезда 27
+                    </p>
+                </div>
+                <div class="socials">
+                    <a href="https://vk.com/freakdancestudio"><i class="fab fa-vk"></i> &nbsp; vk.com/freakdancestudio</a><br/>
+                    <a href="https://www.youtube.com/@freakdancelifetv"><i class="fab fa-youtube"></i> &nbsp; youtube.com/freakdancelifetv</a>
+                    <br/><br/><br/>
+                    <p class="font">
+                        *Информация на сайте не является публичной офертой
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
---
+++ site/app/include/sidebar.php
@@ -0,0 +1,24 @@
+<?php include "../../path.php"; ?>
+<div class="row">
+    <div class="sidebar col-2">
+        <ul>
+            <li>
+                <a href="<?php echo BASE_URL . "admin/questions/index.php";?>">Вопросы</a>
+            </li>
+            <li>
+                <a href="<?php echo BASE_URL . "admin/categories_of_words/index.php";?>">Категории слов для изучения</a>
+            </li>
+            <li>
+                <a href="<?php echo BASE_URL . "admin/categories_for_test/index.php";?>">Категории для тестов</a>
+            </li>
+            <li>
+                <a href="<?php echo BASE_URL . "admin/study_words_in_text/index.php";?>">Слова для изучения</a>
+            </li>
+            <li>
+                <a href="<?php echo BASE_URL . "admin/users/index.php";?>">Пользователи</a>
+            </li>
+            <li>
+                <a href="<?php echo BASE_URL . "admin/answers/index.php";?>">Ответы</a>
+            </li>
+        </ul>
+    </div>
