cbbec9b117243c55a32104ea7efa7cffec62044c
dariamm03 <164412814+dariamm03@users.noreply.github.com>
2024-03-22 01:58:14 +0500

    Add files via upload

---
+++ site/admin/users/index.php
@@ -0,0 +1,48 @@
+<?php
+
+include "../../app/database/db.php";
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Maritime English</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+</html>
+
+
+<!-- connection header -->
+<?php include("../../app/include/header-admin.php"); ?>
+<div class="container">
+    <?php include "../../app/include/sidebar.php"; ?>
+
+
+    <div class="styles col-9">
+    <div>
+        <br><br>
+        <div>
+            <?php Results() ?>
+        </div>
+        <br><br><br>
+    </div>
+    </div>
+</div>
+</div>
+
+<?php include("../../app/include/footer.php"); ?>
+
+
+
+
+
+
+
+
